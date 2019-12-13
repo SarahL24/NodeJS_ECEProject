93d3df84916b4018b37bcf7ee4dd6dc49108db3f
Sarah Lehuby <sarah.lehuby@bbox.fr>
2019-12-13 19:21:31 +0100

    Ajout de mocha et dossier test + fichier travis.yml

---
+++ test/saving_test.js
@@ -0,0 +1,14 @@
+const mocha = require('mocha');
+const assert = require('assert');
+var tmp = require('../index.js');
+
+//Describe tests
+describe("saving records tests", function(){
+
+    //creat tests
+
+    it("saves a record to the DB", function(){
+    });
+
+    
+})
