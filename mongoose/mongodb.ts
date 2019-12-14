782c2e399d422ffeed96ec6e4c525b027da3eb72
Sarah Lehuby <sarah.lehuby@bbox.fr>
2019-12-14 22:15:02 +0100

    MAJ de la db

--- mongoose/mongodb.ts
+++ mongoose/mongodb.ts
@@ -11,24 +11,28 @@ export class MongoDB{
         useUnifiedTopology: true,
     }
 
-    public connect = () => {
+    public connect = new Promise( (resolve, reject) => {
         console.log('Connection to MongoDB...')
         mongoose.connect("mongodb://localhost:27017/nodeProject", this.options)
         .then(()=>{
             console.log('MongoDB is connected to localhost')
-
-        }).catch(err => {
+            resolve('OK')
+        })
+        .catch( (err) => {
             console.log('Unable to connect to local MongoDB, trying to reach mongo container')
 
             mongoose.connect("mongodb://mongo:27017/nodeProject", this.options)
             .then( ()=>{
                 console.log('MongoDB is connected to mongo container')
+                resolve('OK')
             })
             .catch(err => {
                 console.log('Unable to connect to the container MongoDB.')
+                console.log(err)
+                reject('Not OK')
             })
         })
-    }
+    })
 
 }
 
