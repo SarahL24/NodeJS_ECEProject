79bf4c4f13976dc6b5dd92bf46452695e278cb44
Messedi <amir.messedi@edu.ece.fr>
2019-12-15 19:55:13 +0100

    chart

--- src/metrics.ts
+++ src/metrics.ts
@@ -1,9 +1,8 @@
-import {UserMongo} from '../mongoose/user';
-import jwt = require('jsonwebtoken');
-import {User} from './users'
+import { UserMongo } from "../mongoose/user";
+import jwt = require("jsonwebtoken");
+import { User } from "./users";
 
 export class Metric {
-
   public id: string;
   public value: number;
   public date: Date;
@@ -21,43 +20,49 @@ export class MetricsHandler {
 
   // Add a metric of user in db
   public add = async (userToUpdate: User, metric: Metric, callback: any) => {
+    var toUpdate = true;
 
-    var toUpdate = true
+    await this.userModel.find().exec((err: Error, users: any) => {
+      if (err) return console.log(err);
 
-    await this.userModel.find().exec( (err: Error, users: any) => {
-        if(err)
-          return console.log(err);
-
-        if(toUpdate === true){
-            this.userModel.updateOne( {email: userToUpdate.email}, {$push: {metrics: metric}}, (err: Error, user: any) => {
-              if (err) { throw err; }
-              callback(null, metric)
-            })
-        } else {
-            callback(null, null)
-        }
-    })
-  }
+      if (toUpdate === true) {
+        this.userModel.updateOne(
+          { email: userToUpdate.email },
+          { $push: { metrics: metric } },
+          (err: Error, user: any) => {
+            if (err) {
+              throw err;
+            }
+            callback(null, metric);
+          }
+        );
+      } else {
+        callback(null, null);
+      }
+    });
+  };
 
   // Delete a metric in db
   public delete = async (user: User, metricID: string, callback: any) => {
+    var toDelete = true;
 
-    var toDelete = true
+    await this.userModel.find().exec((err: Error, users: any) => {
+      if (err) return console.log(err);
 
-    await this.userModel.find().exec( (err: Error, users: any) => {
-      
-        if(err)
-          return console.log(err);
-
-        if(toDelete === true){
-            this.userModel.updateOne( {email: user.email}, {$pull: {metrics: {_id: metricID}}}, (err: Error, user: any) => {
-              if (err) { throw err; }
-              callback(null, metricID)
-            })
-        } else {
-            callback(null, null)
-        }
-    })
-  }
-
+      if (toDelete === true) {
+        this.userModel.updateOne(
+          { email: user.email },
+          { $pull: { metrics: { _id: metricID } } },
+          (err: Error, user: any) => {
+            if (err) {
+              throw err;
+            }
+            callback(null, metricID);
+          }
+        );
+      } else {
+        callback(null, null);
+      }
+    });
+  };
 }
